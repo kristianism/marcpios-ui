cad48aacf25d638504d92b88cc656bd2b4bb3e53
Kristian <kristianism@pm.me>
2026-03-05 18:10:29 +0800

    Install motion, lenis, gsap and create providers.tsx for the initial package setups

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SmoothScroll } from "./providers";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,11 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <SmoothScroll>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </SmoothScroll>
     </html>
   );
 }
