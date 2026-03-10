c5c2202178f12cee0c779f085cc0f46cf1ec6fd3
Kristian <kristianism@pm.me>
2026-03-10 21:10:58 +0800

    Enable speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { SmoothScroll } from "./providers";
 
 import "./globals.css";
@@ -63,6 +64,7 @@ export default function RootLayout({
         <SmoothScroll>
           {children}
           <Analytics />
+          <SpeedInsights />
         </SmoothScroll>
       </body>
     </html>
